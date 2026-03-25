37ebaa935c794f5046c467ca7084b5c8b5c94e08
lashvarda <lashajincharadze16@gmail.com>
2026-03-26 03:01:09 +0400

    Added main-parts, views, urls

--- mta/urls.py
+++ mta/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='/'),
+    path('leaderboard/', views.leaderboard, name='leaderboard'),
+    path('market/', views.market, name='market'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('profile/', views.profile, name='profile'),
 ]
--- mta/views.py
+++ mta/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'mta/index.html')
+    return render(request, 'mta/index.html', {'title': 'Home'})
+
+def leaderboard(request):
+    return render(request, 'mta/leaderboard.html', {'title': 'Leaderboard'})
+
+def market(request):
+    return render(request, 'mta/market.html', {'title': 'Market'})
+
+def portfolio(request):
+    return render(request, 'mta/portfolio.html', {'title': 'Portfolio'})
+
+def profile(request):
+    return render(request, 'mta/profile.html', {'title': 'Profile'})
